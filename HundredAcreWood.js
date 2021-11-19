97d1c0469958364c57c1e784e2063819ee0469af
JavierVSM <javier.vsm@gmail.com>
2021-11-19 14:21:32 -0600

    first commit

---
+++ HundredAcreWood.js
@@ -0,0 +1,101 @@
+let tigger = { character: "Tigger" };
+let pooh = { character: "Winnie the Pooh" };
+let piglet = { character: "Piglet"};
+let kid = { character: "Christopher Robin" };
+let bees = { character: "Bees" };
+let rabbit = { character: "Rabbit" };
+let owl = { character: "Owl" };
+let eeyore = { character: "Eeyore" };
+let heffalumps = { character: "Heffalump" };
+let kanga = { character: "Kanga" };
+let gopher = { character: "Gopher" };
+
+
+tigger.north = pooh; 
+pooh.south = tigger;
+
+kid.south=tigger.north;
+tigger.north.north = kid;
+
+kanga.south=tigger.north.north;
+tigger.north.north.north=kanga;
+
+eeyore.south=tigger.north.north.north;
+tigger.north.north.north.north=eeyore;
+
+
+piglet.east = tigger.north;
+tigger.north.west = piglet;
+
+owl.east=tigger.north.north;
+tigger.north.north.west=owl;
+owl.south=tigger.north.west;
+piglet.north=tigger.north.north.west;
+
+
+bees.west = tigger.north;
+tigger.north.east = bees;
+
+rabbit.west=tigger.north.north;
+tigger.north.north.east=rabbit;
+rabbit.south=tigger.north.east;
+bees.north=tigger.north.north.west;
+
+
+piglet.north=//owl.south;
+
+
+
+
+
+
+bees.west = tigger.north;
+tigger.north.east = bees;
+bees.north=/rabbit/;
+
+
+let kid = { character: "Christopher Robin" };
+kid.south=tigger.north;
+tigger.north.north=kid.south;
+
+kid.west=;
+kid.east=;
+kid.south=;
+kid.north=;
+
+
+let rabbit = { character: "Rabbit" };
+rabbit.west=tigger.north.north;
+rabbit.east=;
+rabbit.south=;
+rabbit.north=;
+
+let owl = { character: "Owl" };
+owl.west=;
+owl.east=;
+owl.south=;
+owl.north=;
+
+let eeyore = { character: "Eeyore" };
+eeyore.west=;
+eeyore.east=;
+eeyore.south=;
+eeyore.north=;
+
+let heffalumps = { character: "Heffalump" };
+heffalumps.west=;
+heffalumps.east=;
+heffalumps.south=;
+heffalumps.north=;
+
+let kanga = { character: "Kanga" };
+kanga.west=;
+kanga.east=;
+kanga.south=;
+kanga.north=;
+
+let gopher = { character: "Gopher" };
+gopher.west=;
+gopher.east=;
+gopher.south=;
+gopher.north=;
